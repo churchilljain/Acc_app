06eb99763649e2af4a8d786628e45c84708420be
churchilljain <churchilkauchhal@gmail.com>
2024-07-26 03:46:08 +0530

    commit

--- firebase.js
+++
@@ -1,7 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./config/baggu-7d222-firebase-adminsdk-7asjq-660976abd1.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
