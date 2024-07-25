3f00914a8f7cf4c1ceda60d85068dccf64915f88
churchilljain <113435324+churchilljain@users.noreply.github.com>
2024-07-26 03:59:26 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,13 +5,12 @@ import '@firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-//   projectId: "YOUR_PROJECT_ID",
-//   storageBucket: "YOUR_PROJECT_ID.appspot.com",
-//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//   appId: "YOUR_APP_ID",
-//   measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyA7n4xxvRAxcz5GKj1c0ekJJpM_qPKK2H8",
+  authDomain: "baggu-7d222.firebaseapp.com",
+  projectId: "baggu-7d222",
+  storageBucket: "baggu-7d222.appspot.com",
+  messagingSenderId: "12044304174",
+  appId: "1:12044304174:android:fbf264a80c84eac8d6c4bf",
 };
 
 // Initialize Firebase
